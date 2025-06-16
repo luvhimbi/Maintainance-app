369db1cd32efc8814fa7bc72dd856d149e3538be
Talifhani <63749340+luvhimbi@users.noreply.github.com>
2025-06-16 07:04:44 -0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -63,7 +63,7 @@ class ProfileController extends Controller
         $user = Auth::user();
         $admin = Admin::where('user_id', $user->user_id)->first();
 
-        return view('Admin.profile', [
+        return view('admin.profile', [
             'user' => $user,
             'admin' => $admin
         ]);
