a3ee79dbadfb16d5ab8d92afe08873af5d06a8f7
Talifhani <63749340+luvhimbi@users.noreply.github.com>
2025-06-16 06:06:46 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import laravelPlugin from 'laravel-vite-plugin';
 import reactRefresh from '@vitejs/plugin-react';
 
